c89d9c4ee2dfe9a9ce4534cf0c6c53a5bc9a644f
Alisha <alishamirfan1@gmail.com>
2024-03-30 16:17:58 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+let todos = [];
+while (true) {
+    let input = await inquirer.prompt([
+        {
+            name: "Todoitem",
+            type: "input",
+            message: 'add todo item in listing'
+        },
+        {
+            name: "addMore",
+            type: 'list',
+            choices: ["yes", "no"]
+        }
+    ]);
+    const { Todoitem, addMore } = input;
+    todos.push(Todoitem);
+    if (addMore == "no") {
+        console.log("Todolist:");
+        for (let i = 0; i < todos.length; i++) {
+            console.log(todos[i]);
+        }
+        break;
+    }
+}
